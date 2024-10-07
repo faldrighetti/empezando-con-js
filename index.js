0e2783ed8067e4f82927c710aeedb11b148b6d1c
Federico Aldrighetti <f.a.aldrighetti@gmail.com>
2024-10-07 00:44:03 -0300

    Agrega documentación

---
+++ index.js
@@ -0,0 +1,4 @@
+const titulo = document.querySelector('#titulo');
+const texto = document.querySelector('.texto');
+const pie = document.querySelector('#pie');
+const imagen = document.querySelector('img');
